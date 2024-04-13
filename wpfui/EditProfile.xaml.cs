77a6a784aebfba74a092a30265488c240e712041
CataPeGit <catapenet78@gmail.com>
2024-04-14 00:14:51 +0300

    Added Artist UI
    Added Artist UI

---
+++ wpfui/EditProfile.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfui
+{
+    /// <summary>
+    /// Interaction logic for EditProfile.xaml
+    /// </summary>
+    public partial class EditProfile : Window
+    {
+        public EditProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
